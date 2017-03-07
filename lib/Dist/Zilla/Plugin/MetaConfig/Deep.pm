31ffe23c69f955944e8a3fb5e6e90e0a66d22dcc
Kent Fredric <kentfredric@gmail.com>
2017-03-07 22:32:51 +1300

    Build results of 8b19af5 (on master)

--- lib/Dist/Zilla/Plugin/MetaConfig/Deep.pm
+++ lib/Dist/Zilla/Plugin/MetaConfig/Deep.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::MetaConfig::Deep;
 
-our $VERSION = '0.001000';
+our $VERSION = '0.001001';
 
 # ABSTRACT: Experimental enhancements to MetaConfig
 
@@ -98,7 +98,7 @@ Dist::Zilla::Plugin::MetaConfig::Deep - Experimental enhancements to MetaConfig
 
 =head1 VERSION
 
-version 0.001000
+version 0.001001
 
 =head1 DESCRIPTION
 
@@ -148,7 +148,7 @@ Kent Fredric <kentnl@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2015 by Kent Fredric <kentfredric@gmail.com>.
+This software is copyright (c) 2017 by Kent Fredric <kentfredric@gmail.com>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
